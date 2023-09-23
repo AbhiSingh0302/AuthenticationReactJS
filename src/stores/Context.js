fe39a76cedc2eadeee9051487859120cbf02759a
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-09-23 22:47:50 +0530

    token expires in 5 minutes

--- src/stores/Context.js
+++ src/stores/Context.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const Context = createContext({
     isLoggedIn: "",
@@ -10,18 +10,31 @@ const Context = createContext({
 const ContextProvider = props => {
     const initialToken = localStorage.getItem("token");
     const [token, setToken] = useState(initialToken);
-
+    
     const userIsLoggedIn = !!token;
-
+    
     const loginHandler = (token) => {
         setToken(token);
+        localStorage.setItem("time",0);
         localStorage.setItem("token",token);
     }
-
+    
     const logoutHandler = () => {
         setToken(null);
         localStorage.removeItem("token");
+        localStorage.removeItem("time");
     }
+
+    useEffect(() => {
+        const id = setInterval(() => {
+            localStorage.setItem("time",Number(localStorage.getItem("time"))+1);
+            if(Number(localStorage.getItem("time")) === 300){
+                alert("timesup");
+                logoutHandler();
+                clearInterval(id);
+            }
+        },1000)
+    },[])
 
     const context = {
         token: token,
